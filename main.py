a26f5378fd91ccf63f416894cb80f53dc9dc0981
Ubuntu <azureuser@md-mv.3yzdhmye5pye3lsnzt3vamldne.dx.internal.cloudapp.net>
2022-05-18 09:01:21 +0000

    feature: fixing failures

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from utils import create_model
 import pandas as pd
+import json
 
 class Input(BaseModel):
     gender: str
@@ -41,7 +42,20 @@ app = FastAPI()
 model = create_model(loss="binary_crossentropy")
 model.load_weights("training/model_weights.h5")
 
+with open("./codificaciones.json","r") as f:
+    data = json.load(f)
+
 @app.post('/')
-async def home(req: Input):
-    result = model.predict(pd.DataFrame(req))[0][0] > 0.23
-    return {'resultado': result}
+async def home(req: Input = Body(...)): 
+    req = req.dict()
+    req["gender"] = data["gender"].index(req["gender"])
+    req["job"] = data["job"].index(req["job"])
+    req["city"] = data["city"].index(req["city"])
+    req["state"] = data["state"].index(req["state"])
+    req["category"] = data["category"].index(req["category"])
+    req["merchant"] = data["merchant"].index(req["merchant"])
+    req["card_type"] = data["card_type"].index(req["card_type"])
+    req["card_industry"] = data["card_industry"].index(req["card_industry"])
+    result = float(model.predict(pd.DataFrame([req]))[0][0])
+    result = result > 0.23
+    return {'is_fraud': result}
